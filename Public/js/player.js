6a087ebf3971b6d0db8324fbd1fdc44e7f3615f1
Nathan Looney <nathanalooney@gmail.com>
2015-12-23 21:08:22 -0500

    Songlist is rendering fake json data

--- Public/js/player.js
+++ Public/js/player.js
@@ -1,5 +1,49 @@
+var inputData = [{result: 1, title: "Song 1", artist: "Artist 1"},
+	{result: 2, title: "Song 2", artist: "Artist 2"},
+	{result: 3, title: "Song 3", artist: "Artist 3"},
+	{result: 4, title: "Song 4", artist: "Artist 4"}];
+
+var LibraryBox = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1> SCLibrary </h1>
+        <SongList data={this.props.data}/>
+      </div>
+    );
+  }
+});
+
+var Song = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<h3> {this.props.artist} </h3>
+				<p> {this.props.title} </p>
+			</div>
+		);
+	}
+});
+
+var SongList = React.createClass({
+    render: function() {
+
+      var songNodes = this.props.data.map(function(song) {
+        return (
+          <Song artist={song.artist} title={song.title}>
+          </Song>
+        );
+      });
+
+      return (
+        <div className="songList">
+          {songNodes}
+        </div>
+      );
+    }
+})
+
 ReactDOM.render(
-<h1>Hello, world!</h1>,
-document.getElementById('example')
+  <LibraryBox data={inputData}/>,
+  document.getElementById('main')
 );
-console.log("Rendered");
