b1e861a0aa47397385f7389515c434f16395711a
Nathan Looney <nathanalooney@gmail.com>
2016-01-06 20:17:24 -0500

    Stable build

--- Public/js/player.js
+++ Public/js/player.js
@@ -1,49 +1,49 @@
-// var inputData = [{result: 1, title: "Song 1", artist: "Artist 1"},
-// 	{result: 2, title: "Song 2", artist: "Artist 2"},
-// 	{result: 3, title: "Song 3", artist: "Artist 3"},
-// 	{result: 4, title: "Song 4", artist: "Artist 4"}];
+var inputData = [{result: 1, title: "Song 1", artist: "Artist 1"},
+	{result: 2, title: "Song 2", artist: "Artist 2"},
+	{result: 3, title: "Song 3", artist: "Artist 3"},
+	{result: 4, title: "Song 4", artist: "Artist 4"}];
 
-// var LibraryBox = React.createClass({
-//   render: function() {
-//     return (
-//       <div>
-//         <h1> SCLibrary </h1>
-//         <SongList data={this.props.data}/>
-//       </div>
-//     );
-//   }
-// });
+var LibraryBox = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1> SCLibrary </h1>
+        <SongList data={this.props.data}/>
+      </div>
+    );
+  }
+});
 
-// var Song = React.createClass({
-// 	render: function() {
-// 		return (
-// 			<div>
-// 				<h3> {this.props.artist} </h3>
-// 				<p> {this.props.title} </p>
-// 			</div>
-// 		);
-// 	}
-// });
+var Song = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<h3> {this.props.artist} </h3>
+				<p> {this.props.title} </p>
+			</div>
+		);
+	}
+});
 
-// var SongList = React.createClass({
-//     render: function() {
+var SongList = React.createClass({
+    render: function() {
 
-//       var songNodes = this.props.data.map(function(song) {
-//         return (
-//           <Song artist={song.artist} title={song.title}>
-//           </Song>
-//         );
-//       });
+      var songNodes = this.props.data.map(function(song) {
+        return (
+          <Song artist={song.artist} title={song.title}>
+          </Song>
+        );
+      });
 
-//       return (
-//         <div className="songList">
-//           {songNodes}
-//         </div>
-//       );
-//     }
-// })
+      return (
+        <div className="songList">
+          {songNodes}
+        </div>
+      );
+    }
+})
 
-// ReactDOM.render(
-//   <SongList data={inputData}/>,
-//   document.getElementById('main')
-// );
+ReactDOM.render(
+  <SongList data={inputData}/>,
+  document.getElementById('main')
+);
