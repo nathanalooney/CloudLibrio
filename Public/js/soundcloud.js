e4e60fd7999e0e5223142b8feb8c2e5d1b9737d4
Nathan Looney <nal3vm@virginia.edu>
2016-01-06 20:17:24 -0500

    Stable build

--- Public/js/soundcloud.js
+++ Public/js/soundcloud.js
@@ -1,26 +1,4 @@
-// SC.initialize({
-// 	client_id: '96089e67110795b69a95705f38952d8f',
-// 	redirect_uri: 'http://sclibrary.testing.com:3000/callback.html',
-// });
-
-// SC.connect(function() {
-// 	console.log("Connected!");
-// })
-// // SC.get('/resolve', {url: 'https://soundcloud.com/iamganz/snakehips-x-tory-lanez-dimelo-ganz-flip-free-dl'}).then(function(track) {
-// // 	console.log(track);
-// // });
-
-  $(document).ready(function() {
-      SC.initialize({
-          client_id: '96089e67110795b69a95705f38952d8f',
-          redirect_uri: 'sclibrary.herokuapp.com/callback.html'
-      });
-
-
-      SC.connect(function() {
-      	  console.log("FUCK");
-          SC.get('/me', function(data) {
-              $('#main').text(data.username);
-          });
-      });
-  });
+SC.initialize({
+	client_id: '96089e67110795b69a95705f38952d8f',
+	redirect_uri: 'http://sclibrary.testing.com:3000/callback.html',
+});
