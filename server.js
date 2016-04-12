1335e6fa2b903d800b47c92e764a51d1415db84c
Nathan Looney <nathanalooney@gmail.com>
2016-04-12 14:12:46 -0400

    herokuuu

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
-app.use(express.static(__dirname + '/Public'));
+
+app.use(express.static(__dirname + '/Public', {
+	setHeaders: function(res, path, stat) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "origin, x-requested-with, content-type, accept, accept-encoding, authorization, x-csrftoken, accept-language");
+	  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	}
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-console.log(__dirname);
-console.log(require('fs').readdirSync(__dirname));
-
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
