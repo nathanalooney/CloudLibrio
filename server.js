f65a30c353b5b5d7572eaf7789b002b7e373c71f
Nathan Looney <nathanalooney@gmail.com>
2016-04-12 12:10:06 -0400

    Changed listen setting for heroku to test

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+process.env.PWD = process.cwd();
+
+
 var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(process.env.PWD, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
