fc4b4d5cb5b2d7fec555573b12d7da09b41f7d0e
Nathan Looney <nathanalooney@gmail.com>
2016-04-12 12:26:36 -0400

    updated package.json

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-process.env.PWD = process.cwd();
+var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static(path.join(process.env.PWD, '/public')));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+console.log(__dirname); console.log(require('fs').existsSync(__dirname + '/public'))
 
-app.listen(app.get('port'), '0.0.0.0', function() {
+app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
