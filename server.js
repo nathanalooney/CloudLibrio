2edf3a3ce0dabe68a8777bb3e4bf33e31b1c7e60
Nathan Looney <nathanalooney@gmail.com>
2016-04-12 12:33:50 -0400

    updated package.json

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+console.log(__dirname);
+console.log(require('fs').readdirSync(__dirname));
 
-console.log(__dirname); console.log(require('fs').existsSync(__dirname + '/public'))
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
