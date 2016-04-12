8d1961b71a9e06493bcf2d9bbf52e6c6e2d9d528
Nathan Looney <nathanalooney@gmail.com>
2016-04-12 13:26:33 -0400

    updated package.json

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
 app.set('port', (process.env.PORT || 3000));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(__dirname + '/Public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
