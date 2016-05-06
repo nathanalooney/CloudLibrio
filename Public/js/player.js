8ad4a0254181e1bb131b487a595aad21ca2f5208
Nathan Looney <nathanalooney@gmail.com>
2016-05-05 21:57:13 -0400

    Working on CSV

--- Public/js/player.js
+++ Public/js/player.js
@@ -600,7 +600,57 @@
         document.body.className = '';     
     });
 
+$("#download-csv").click(function() {
+    var json = $.parseJSON($("#json").val());
+    var csv = JSON2CSV(json);
+    window.open("data:text/csv;charset=utf-8," + escape(csv))
+});
+
+
     //-------------------------------------------------------------------------------------------------------------//
+
+    function JSON2CSV(objArray) {
+        var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+
+        var str = '';
+        var line = '';
+
+        if ($("#labels").is(':checked')) {
+            var head = array[0];
+            if ($("#quote").is(':checked')) {
+                for (var index in array[0]) {
+                    var value = index + "";
+                    line += '"' + value.replace(/"/g, '""') + '",';
+                }
+            } else {
+                for (var index in array[0]) {
+                    line += index + ',';
+                }
+            }
+
+            line = line.slice(0, -1);
+            str += line + '\r\n';
+        }
+
+        for (var i = 0; i < array.length; i++) {
+            var line = '';
+
+            if ($("#quote").is(':checked')) {
+                for (var index in array[i]) {
+                    var value = array[i][index] + "";
+                    line += '"' + value.replace(/"/g, '""') + '",';
+                }
+            } else {
+                for (var index in array[i]) {
+                    line += array[i][index] + ',';
+                }
+            }
+
+            line = line.slice(0, -1);
+            str += line + '\r\n';
+        }
+        return str; 
+    }
 
     var clearVisualizer = function() {
         var svg = document.getElementsByTagName('svg')[0];
